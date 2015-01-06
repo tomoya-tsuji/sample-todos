603241a2f863cf33df8429f281b83ad69a3dab26
Tomoya Tsuji <1020tomoya@gmail.com>
2015-01-06 16:29:47 +0900

    step2.Templates

--- simple-todos.js
+++ simple-todos.js
@@ -1,21 +1,22 @@
+//===========================
+//クライアント側のみで動くコード
+//===========================
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
+  
+  Template.body.helpers({
+    tasks:[
+       { text:"This is task 1"},
+       { text:"This is task 2"},
+       { text:"This is task 3"}
+    ]
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+ 
 }
 
+
+//===========================
+//サーバー側のみで動くコード
+//===========================
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
